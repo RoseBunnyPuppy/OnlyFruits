3475fd5af75a6be52edce691d137a0774544a290
RoseBunnyPuppy <rosebunnypuppy@proton.me>
2026-06-30 16:25:48 -0700

    Added some asset logging extensions

---
+++ OnlyFruitsMod/Extensions/IMonitorExtensions.cs
@@ -0,0 +1,33 @@
+﻿using StardewModdingAPI;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace OnlyFruitsMod.Extensions
+{
+    public static class IMonitorExtensions
+    {
+        public static void LogAssetReady(this IMonitor monitor, object source, string? assetName) =>
+            monitor.LogDebug($"[{source.GetType().Name}] {assetName} (AssetReady)");
+        public static void LogAssetReady(this IMonitor monitor, object source, IAssetName? assetName) =>
+            monitor.LogAssetReady(source, assetName?.ToString());
+
+
+        public static void LogAssetRequested(this IMonitor monitor, object source, string? assetName) =>
+            monitor.LogDebug($"[{source.GetType().Name}] {assetName} (AssetRequested)");
+        public static void LogAssetRequested(this IMonitor monitor, object source, IAssetName? assetName) =>
+            monitor.LogAssetRequested(source, assetName?.ToString());
+
+
+        public static void LogAssetInvalidated(this IMonitor monitor, object source, string? assetName) =>
+            monitor.LogDebug($"[{source.GetType().Name}] {assetName} (AssetInvalidated)");
+        public static void LogAssetInvalidated(this IMonitor monitor, object source, IAssetName? assetName) =>
+            monitor.LogAssetInvalidated(source, assetName?.ToString());
+
+
+        public static void LogDebug(this IMonitor monitor, string content, LogLevel? level = default)
+        {
+            monitor.Log(content, LogLevel.Debug);
+            Debug.WriteLine(content);
+        }
+    }
+}
