605b6011ab494e7481c879e63f15a6ec4d53860f
RoseBunnyPuppy <rosebunnypuppy@proton.me>
2026-07-01 11:45:04 -0700

    Created a singleton logger so i dont have to pass monitor everywhere

---
+++ OnlyFruitsMod/Features/Logging/Logger.cs
@@ -0,0 +1,25 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyFruitsMod.Features.Logging
+{
+    /// <summary>
+    ///     Provides access to a singleton logger so I dont have
+    ///   to pass around a <see cref="IMonitor"/> everywhere.
+    /// </summary>
+    public class Logger
+    {
+        public static Logger Instance { get; } = new Logger();
+
+        private IMonitor? monitor;
+        public IMonitor Monitor => monitor ?? throw new InvalidOperationException("No IMonitor is configured.");
+        public void SetMonitor(IMonitor monitor)
+        {
+            this.monitor = monitor;
+        }
+    }
+}
