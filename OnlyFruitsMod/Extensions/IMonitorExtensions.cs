59311adc5d462d061e1c26d4e9cf2d2e7a4466d1
RoseBunnyPuppy <rosebunnypuppy@proton.me>
2026-06-30 17:10:21 -0700

    Made the Asset(Invalidated|Requested|Ready) logs gated by the DisableDevHelpers preprocess directive

--- OnlyFruitsMod/Extensions/IMonitorExtensions.cs
+++ OnlyFruitsMod/Extensions/IMonitorExtensions.cs
@@ -6,20 +6,32 @@ namespace OnlyFruitsMod.Extensions
 {
     public static class IMonitorExtensions
     {
-        public static void LogAssetReady(this IMonitor monitor, object source, string? assetName) =>
+        public static void LogAssetReady(this IMonitor monitor, object source, string? assetName)
+        {
+#if !DisableDevHelpers
             monitor.LogDebug($"[{source.GetType().Name}] {assetName} (AssetReady)");
+#endif
+        }
         public static void LogAssetReady(this IMonitor monitor, object source, IAssetName? assetName) =>
             monitor.LogAssetReady(source, assetName?.ToString());
 
 
-        public static void LogAssetRequested(this IMonitor monitor, object source, string? assetName) =>
+        public static void LogAssetRequested(this IMonitor monitor, object source, string? assetName)
+        {
+#if !DisableDevHelpers
             monitor.LogDebug($"[{source.GetType().Name}] {assetName} (AssetRequested)");
+#endif
+        }
         public static void LogAssetRequested(this IMonitor monitor, object source, IAssetName? assetName) =>
             monitor.LogAssetRequested(source, assetName?.ToString());
 
 
-        public static void LogAssetInvalidated(this IMonitor monitor, object source, string? assetName) =>
+        public static void LogAssetInvalidated(this IMonitor monitor, object source, string? assetName)
+        {
+#if !DisableDevHelpers
             monitor.LogDebug($"[{source.GetType().Name}] {assetName} (AssetInvalidated)");
+#endif
+        }
         public static void LogAssetInvalidated(this IMonitor monitor, object source, IAssetName? assetName) =>
             monitor.LogAssetInvalidated(source, assetName?.ToString());
 
@@ -27,7 +39,9 @@ namespace OnlyFruitsMod.Extensions
         public static void LogDebug(this IMonitor monitor, string content, LogLevel? level = default)
         {
             monitor.Log(content, LogLevel.Debug);
+#if !DisableDevHelpers
             Debug.WriteLine(content);
+#endif
         }
     }
 }
