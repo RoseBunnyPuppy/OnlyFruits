54c98296c16ac1a60378d08ef24acd6446546c0c
RoseBunnyPuppy <rosebunnypuppy@proton.me>
2026-07-01 09:40:59 -0700

    Added documentation, removed unused OnlyFruitsSlayMonsterQuest class

--- OnlyFruitsMod/Features/Quests/FieldValueEventHandlers.cs
+++ OnlyFruitsMod/Features/Quests/FieldValueEventHandlers.cs
@@ -4,6 +4,12 @@ namespace OnlyFruitsMod.Features.Quests
 {
     public static class FieldValueEventHandlers
     {
+        /// <summary>
+        ///     Updates the value of <paramref name="field"/> to always be
+        ///   <paramref name="oldValue"/> whenever it is updated.  This 
+        ///   handler should NOT cause an infinite loop since the
+        ///   handler is removed before restoring the old value.
+        /// </summary>
         public static void PreserveOldValue(NetString field, string oldValue, string newValue)
         {
             if (oldValue == newValue) return;
@@ -12,6 +18,12 @@ namespace OnlyFruitsMod.Features.Quests
             field.fieldChangeEvent += PreserveOldValue;
         }
 
+        /// <summary>
+        ///     Updates the value of <paramref name="field"/> to always be
+        ///   <paramref name="oldValue"/> whenever it is updated.  This 
+        ///   handler should NOT cause an infinite loop since the
+        ///   handler is removed before restoring the old value.
+        /// </summary>
         public static void PreserveOldValue(NetInt field, int oldValue, int newValue)
         {
             if (oldValue == newValue) return;
@@ -20,10 +32,18 @@ namespace OnlyFruitsMod.Features.Quests
             field.fieldChangeEvent += PreserveOldValue;
         }
 
+        /// <summary>
+        ///     Updates the value of <paramref name="field"/> to always be
+        ///   0 whenever it is updated.  This 
+        ///   handler should NOT cause an infinite loop since the
+        ///   handler is removed before restoring the old value.
+        /// </summary>
         public static void ResetFieldToZeroHandler(NetInt field, int oldValue, int newValue)
         {
             if (oldValue == newValue) return;
+            field.fieldChangeEvent -= ResetFieldToZeroHandler;
             field.Value = 0;
+            field.fieldChangeEvent += ResetFieldToZeroHandler;
         }
     }
 }
